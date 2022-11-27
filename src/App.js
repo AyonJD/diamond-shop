d022844d873ce545c90a19ca931d0c34f09a2fa4
Ayon Jodder <ayonjodder177@gmail.com>
2022-11-27 11:23:28 +0600

    Footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 import TopupPage from './Pages/TouupPage/TopupPage';
 import ShopPage from './Pages/ShopPage/ShopPage';
 import ContactPage from './Pages/ContactPage/ContactPage';
+import Footer from './Components/Shared/Footer';
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
         <Route path='/topup' element={<TopupPage />} />
         <Route path='/shop' element={<ShopPage />} />
         <Route path='/contact' element={<ContactPage />} />
-       </Routes>
+      </Routes>
+      <Footer />
     </div>
   );
 }
